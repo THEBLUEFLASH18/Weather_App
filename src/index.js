d28d9dc34c6c9d461c85930f98973ba088951f93
THEBLUEFLASH18 <68237204+THEBLUEFLASH18@users.noreply.github.com>
2024-07-04 13:52:57 -0400

    Added settings for Rainy days weather

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ import { Application } from '@splinetool/runtime';
 
 const canvas = document.getElementById('canvas3d');
 const app = new Application(canvas);
-app.load('https://prod.spline.design/p9FyhN5Pd0xgAFwl/scene.splinecode');
+app.load('https://prod.spline.design/kD00Q0lD47kFlGUA/scene.splinecode');
