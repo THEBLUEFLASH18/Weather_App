f6ab6c579c623ccaf9714676bec0924f06dec048
THEBLUEFLASH18 <68237204+THEBLUEFLASH18@users.noreply.github.com>
2024-07-21 14:39:13 -0400

    Added the necessary code for to receive the weather for specific cities

--- src/index.js
+++ src/index.js
@@ -7,3 +7,26 @@ import { Application } from '@splinetool/runtime';
 const canvas = document.getElementById('canvas3d');
 const app = new Application(canvas);
 app.load('https://prod.spline.design/kD00Q0lD47kFlGUA/scene.splinecode');
+
+
+const city = document.getElementById('input');
+const apiKey = '74c0bd858f2cf11b2ad2cbe09f247d59'
+
+const submitButton = document.getElementById('submit')
+submitButton.addEventListener('click', async function weatherInfo(){
+
+    const cityValue = city.value
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}`
+
+    const weatherData = await fetch(url)
+    if(weatherData.ok){
+        const newData = await weatherData.json()
+        console.log(newData)
+    }
+    else{
+        console.log('Error detailing data')
+    }
+});
+
+
+
